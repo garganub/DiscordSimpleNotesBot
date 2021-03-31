c2ff04e80c8a48fa8cd23ef2f3dffd3c3f92facf
Anubhav Garg <anubhavgarg@Anubhavs-MacBook-Pro.local>
2021-03-31 16:18:33 -0700

    Tutorial files for Discord Bot

---
+++ index.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (message.content === '!ping') {
+	// send back "Pong." to the channel the message was sent in
+	message.channel.send('Pong.');
+}
+});
+
+client.login(process.env.TOKEN);
